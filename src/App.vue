30470b932b71461688b6a72aacd4fc89b2b3965b
giuliogallo94 <giuliogallo1994@gmail.com>
2023-11-27 14:10:16 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script>
+import AppHeader from "./components/AppHeader.vue";
+export default {
+  data() {
+    return {
+      // store,
+    };
+  },
+  created() {
+    // this.store.loading = true;
+    // axios.get(this.store.apiUrl).then((resp) => {
+    //   this.store.characters = resp.data.results;
+    //   this.store.loading = false;
+    // });
+  },
+  components: { AppHeader },
+};
+</script>
+
+<template><AppHeader /></template>
+
+<style lang="scss" scoped>
+@use "./style/general.scss";
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,32 @@
+<script>
+export default {};
+</script>
+
+<template>
+  <header class="py-3 d-flex align-items-center">
+    <div class="logo text-center">
+      <img src="../assets/img/logo.png" alt="" />
+    </div>
+    <div class="title">
+      <h1>Yu-Gi-Oh Api</h1>
+    </div>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+@use "../style/general.scss";
+// @use "../style/partials/variables" as *;
+
+header {
+  background-color: white;
+  width: 100vw;
+
+  .logo {
+    width: 150px;
+
+    img {
+      max-width: 60%;
+    }
+  }
+}
+</style>
